584d4aeb4fb581ed04fcccddfab473b81476d6cf
1mbryk <95372025+1mbryk@users.noreply.github.com>
2025-02-27 17:09:38 +0300

    Initial Commit

---
+++ Diploma/DiplomaApp.swift
@@ -0,0 +1,17 @@
+//
+//  DiplomaApp.swift
+//  Diploma
+//
+//  Created by Mацвей Пажытных on 27.02.25.
+//
+
+import SwiftUI
+
+@main
+struct DiplomaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
